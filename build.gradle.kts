2179a2176913e00a26bad8dc333edb76d37ad5e8
flover-luffy <96760096+flover-luffy@users.noreply.github.com>
2025-06-11 12:55:05 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ version = "0.1.11-dev7"
 
 repositories {
     maven("https://maven.aliyun.com/repository/public")
-    maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+    maven("https://mcl.repo.mamoe.net/")
     mavenCentral()
 }
 
